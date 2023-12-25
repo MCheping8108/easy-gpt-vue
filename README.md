9d160f7f9c1b0baa124ea686bb7df9025bdb5f48
MCheping8108 <liuyiming53@outlook.com>
2023-12-25 21:50:44 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# 🌟easy-gpt-vue
+利用vite+vue写出来简易的GPT网页
+
+# 🚀快速开始
+## 🏠本地部署
+下载代码
+```bash
+git clone git@github.com:MCheping8108/easy-gpt-vue.git
+```
+
+安装依赖
+```bash
+npm install
+```
+
+在本地开发(仅适用于开发)
+```bash
+npm run dev
+```
+
+编译
+```bash
+npm run build
+```
+
+开启服务器
+```bash
+npm run preview
+```
+
+# 📷截图
+![Alt text](./docs/images/image.png)
---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts" >
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+  baseURL: 'https://example.com/',
+  apiKey: 'sk-xxx', // This is the default and can be omitted
+  dangerouslyAllowBrowser: true
+});
+
+async function main() {
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [{ role: 'user', content: '你好，你是来自哪个模型？' }],
+    model: 'gpt-3.5-turbo',
+  });
+  // console.log(chatCompletion.choices[0].message?.content);
+  const content = document.querySelector('.content') as HTMLDivElement
+  const messageContent = chatCompletion.choices[0].message?.content;
+  if (messageContent !== null && messageContent !== undefined) {
+    content.innerHTML = messageContent;
+  }
+}
+
+main();
+
+</script>
+
+<template>
+<div class="hander"><h1>EASY-GPT-VUE</h1></div>
+
+<div class="content">
+  
+</div>
+
+</template>
+
+<style scoped>
+.hander {
+  text-align: center;
+  margin-top: 100px;
+  font-size: 50px;
+  color: #f00;
+}
+
+.content {
+  border: 5px solid black;
+  box-shadow: 10px 10px 5px #888;
+  /* text-align: center; */
+  margin-top: 100px;
+  font-size: 30px;
+}
+</style>
