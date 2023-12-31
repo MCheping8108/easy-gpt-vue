efbdc8ee61e315a63994fc8e9dae4e00b66c9bfa
MCheping8108 <liuyiming53@outlook.com>
2023-12-31 19:00:33 +0800

    利用vue支持环境变量

--- src/components/template_setting.vue
+++ src/components/template_setting.vue
@@ -1,10 +1,6 @@
 <script lang="ts">
 export default {
-    data() {
-        return {
-            base_url: 'https://api.nextapi.fun',
-            api_key: 'sk-xxx',
-        }
-    }
-}
+  base_url: "https://api.example.com",
+  api_key: "sk-xxx",
+};
 </script>
